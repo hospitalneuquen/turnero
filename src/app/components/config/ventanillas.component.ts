e658579f16a55306d315f003d41033eabc29996c
Andrés Velazquez <avelazquez@hospitalneuquen.org.ar>
2017-05-22 08:59:16 -0300

    Ventanillas con monitor sse

--- src/app/components/config/ventanillas.component.ts
+++ src/app/components/config/ventanillas.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { VentanillasService } from './../../services/ventanillas.service';
 import { TurnosService } from './../../services/turnos.service';
 import { IVentanillas } from './../../interfaces/IVentanillas';
-
+declare var EventSource: any;
 @Component({
     selector: 'app-ventanillas',
     templateUrl: 'ventanillas.html'
@@ -15,13 +16,39 @@ export class ListaVentanillasComponent implements OnInit {
     private ventanillaSeleccionada: any = [];
     private ventanillas: any;
     private showEditarVentanillaPanel = false;
-
+    private mensajesServidor: any = {};
     private servidorCaido = false;
+    ws: any;
+    previo: any;
+
+    private EVENT_URL = 'http://localhost:1337/api/update';
+    private message: String = '0';
 
     constructor(private VentanillasService: VentanillasService, private TurnosService: TurnosService,
-        private router: Router, private route: ActivatedRoute) { }
+        private router: Router, private route: ActivatedRoute, private changeDetector: ChangeDetectorRef) { }
 
     ngOnInit() {
+
+        // Creates event object
+        this.ws = new EventSource(this.EVENT_URL, { withCredentials: false });
+
+        // Listing to server messages
+        // this.ws.onmessage = (evt) => {
+
+        //     // Update the model
+        //     this.mensajesServidor = JSON.parse(evt.data);
+
+        //     if (this.mensajesServidor.result !== this.previo) {
+        //         alert('cambió el servidor!')
+        //     } else {
+        //         this.previo = this.mensajesServidor.result;
+        //     }
+
+        //     // Manually detect changes
+        //     this.changeDetector.detectChanges();
+
+        // };
+
         this.inicializarVentanillas();
     }
 
--- src/app/components/ventanilla/ventanilla.component.ts
+++ src/app/components/ventanilla/ventanilla.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { VentanillasService } from './../../services/ventanillas.service';
 import { TurnosService } from './../../services/turnos.service';
 import { IVentanillas } from './../../interfaces/IVentanillas';
-
+declare var EventSource: any;
 @Component({
   selector: 'app-ventanilla',
   templateUrl: './ventanilla.component.html',
@@ -19,18 +19,51 @@ export class VentanillaComponent implements OnInit {
   private noPrioritario: any;
   private turnoActualPrioritario: any;
   private turnoActualNoPrioritario: any;
+  private mensajesServidor: any = {};
+  private eventSource: any;
+  private mensajePrevio: false;
+
+  private EVENT_URL = 'http://localhost:1337/api/update';
+  private message: String = '0';
 
   constructor(private VentanillasService: VentanillasService, private TurnosService: TurnosService,
-    private router: Router, private route: ActivatedRoute) { }
+    private router: Router, private route: ActivatedRoute, private changeDetector: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
+
+    // Crea un nuevo objeto EventSource que viene de la API
+    this.eventSource = new EventSource(this.EVENT_URL, { withCredentials: false });
+
+    // Para la oreja a los mensajes de servidor
+    this.eventSource.onmessage = (evt) => {
+
+      // Se actualiza el mensaje de servidor
+      this.mensajesServidor = JSON.parse(evt.data);
+
+      // let interval = setInterval(() => {
+
+      // }, 2000);
+
+      // Detector de cambios: El último mensaje de la API es diferente al previo?
+      if (this.mensajePrevio && this.mensajesServidor.result !== this.mensajePrevio) {
+        alert('Cambió el servidor!');
+        this.mensajePrevio = false;
+        this.inicializarVentanilla();
+      } else {
+        this.mensajePrevio = this.mensajesServidor.result;
+      }
+
+      // Manually detect changes
+      this.changeDetector.detectChanges();
+
+    };
 
     this.route.params.subscribe(params => {
       // obtenemos el parametro del nombre de la ventanilla enviado
       this.slug = params['slug'];
 
       // TODO: si no tenemos slug redireccionamos a la seleccion de ventanilla
-
       this.inicializarVentanilla();
     });
 
@@ -59,33 +92,33 @@ export class VentanillaComponent implements OnInit {
 
   inicializarTurneros() {
     // obtenemos prioritario
-      this.TurnosService.get({ 'tipo': 'prioritario', 'ultimoEstado': 'uso' }).subscribe(turnos => {
-        // TODO: Revisar de enviar limit : 1
-        if (turnos[turnos.length-1]) {
-          this.prioritario = turnos[turnos.length-1];
+    this.TurnosService.get({ 'tipo': 'prioritario', 'ultimoEstado': 'uso' }).subscribe(turnos => {
+      // TODO: Revisar de enviar limit : 1
+      if (turnos[turnos.length - 1]) {
+        this.prioritario = turnos[turnos.length - 1];
 
-          this.TurnosService.getActual(this.prioritario._id, this.ventanilla._id).subscribe(actual => {
-            console.log(actual);
-            console.log('ACTUAL::::::::::::::::::::::');
-            this.turnoActualPrioritario = actual[0];
+        this.TurnosService.getActual(this.prioritario._id, this.ventanilla._id).subscribe(actual => {
+          console.log(actual);
+          console.log('ACTUAL::::::::::::::::::::::');
+          this.turnoActualPrioritario = actual[0];
 
-            console.log(this.turnoActualPrioritario);
-          });
+          console.log(this.turnoActualPrioritario);
+        });
 
-        }
-      });
+      }
+    });
 
-      // obtenemos no prioritario
-      this.TurnosService.get({ 'tipo': 'no-prioritario', 'ultimoEstado': 'uso' }).subscribe(turnos => {
-        if (turnos[turnos.length-1]) {
-          this.noPrioritario = turnos[turnos.length-1];
+    // obtenemos no prioritario
+    this.TurnosService.get({ 'tipo': 'no-prioritario', 'ultimoEstado': 'uso' }).subscribe(turnos => {
+      if (turnos[turnos.length - 1]) {
+        this.noPrioritario = turnos[turnos.length - 1];
 
-          this.TurnosService.getActual(this.noPrioritario._id, this.ventanilla._id).subscribe(actual => {
-            this.turnoActualNoPrioritario = actual[0];
-          });
+        this.TurnosService.getActual(this.noPrioritario._id, this.ventanilla._id).subscribe(actual => {
+          this.turnoActualNoPrioritario = actual[0];
+        });
 
-        }
-      });
+      }
+    });
   }
 
   /* Pausar ventanilla */
@@ -100,7 +133,6 @@ export class VentanillaComponent implements OnInit {
   /* Reanudar ventanilla */
   reanudar() {
     const params = { pausa: false };
-
     this.updateVentanilla(params);
   }
 
