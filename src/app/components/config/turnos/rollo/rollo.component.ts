6e8d75f4da37e3ab5254e4d131cb357c2ea05283
manouman <manurbano.dev@gmail.com>
2017-11-22 10:27:23 -0300

    Armado de un rollo de turnos completo de la A a la E

---
+++ src/app/components/config/turnos/rollo/rollo.component.ts
@@ -0,0 +1,51 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { TurnosService } from './../../../../services/turnos.service';
+import { IAlert } from './../../../../interfaces/IAlert';
+
+@Component({
+  selector: 'app-rollo',
+  templateUrl: './rollo.component.html',
+  styleUrls: ['./rollo.component.css']
+})
+export class RolloComponent implements OnInit {
+  public colors: any[] = ['amarillo', 'celeste', 'rosado', 'verde', 'violeta'];
+  public turnos: any = {};
+
+  public alert: IAlert;
+
+  constructor(private router: Router, private turnosService: TurnosService) { }
+
+  ngOnInit() {
+  }
+
+  save(isValid: boolean) {
+
+    if (isValid) {
+
+      this.turnosService.nuevoRollo(this.turnos).subscribe(turnos => {
+        //this.onEditEmit.emit(turnos);
+      }, err => {
+        if (err) {
+          const error = JSON.parse(err._body);
+
+          this.alert = {
+            message: error.message,
+            class: 'danger'
+          };
+
+          setTimeout(() => {
+            this.alert = null;
+          }, 10000);
+        }
+      });
+
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['config/turnos']);
+  }
+
+}
